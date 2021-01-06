868befa9d8c1ec4b18537a59a5a83d9892074b67
Scott <scott@swlt.ru>
2021-01-06 14:43:01 +0300

    Обновлен роутер

--- Router.js
+++ Router.js
@@ -39,10 +39,14 @@ class Router {
   /**
    * Получить маршрут
    * @param {String} method 
+   * @param {String} host 
    * @param {String} path 
+   * @return {Object}
    */
-  getRoute(method, path) {
+  getRoute(method, host, path) {
     method = String(method).toUpperCase()
+    host = String(host).toLowerCase()
+    path = String(path).toLowerCase()
 
     // Найти первый подходящий маршрут
     const route = this.routes.find(route => {
@@ -51,6 +55,11 @@ class Router {
        * @return {Boolean}
        */
       const matchMethod = () => String(route.method).toUpperCase() === method
+      /**
+       * Сопоставить хост
+       * @return {Boolean}
+       */
+      const matchHost = () => !route.host || String(route.host).toLowerCase() === host
 
       /**
        * Сопоставить путь
@@ -59,28 +68,52 @@ class Router {
       const matchPath = () => new RegExp(`^${route.path}$`).test(path)
 
       // Если совпадают и метод и путь
-      return matchMethod() && matchPath()
+      return matchMethod() && matchHost() && matchPath()
     })
 
     // Если маршрут найден
     if (route) {
-      return this.normalizeRoute(route)
+      const params = this.getPathParams(path, route.path)
+
+      // Вернуть маршрут
+      return this.normalizeRoute({ ...route, method, host, path, params })
     }
 
     // Вернуть маршрут по умолчанию
-    return this.normalizeRoute({ method, path })
+    return this.normalizeRoute({ method, host, path })
   }
 
   /**
    * Нормализовать маршрут
    * @param {Object} route 
+   * @return {Object}
    */
   normalizeRoute(route) {
     const headers = route.headers || this.defaultHeaders
     const handler = route.handler || this.defaultHandler
     const errorHandler = route.errorHandler || this.defaultErrorHandler
+    const method = route.method || ""
+    const host = route.host || ""
+    const path = route.path || ""
+    const params = route.params || {}
 
-    return { ...route, headers, handler, errorHandler }
+    return { ...route, headers, method, host, path, params, handler, errorHandler }
+  }
+
+  /**
+   * Получить параметры из пути
+   * @param {String} path 
+   * @param {String} pattern 
+   * @return {Object}
+   */
+  getPathParams(path, pattern) {
+    const { groups, index, input, ...matches } = path.match(new RegExp(`^${pattern}$`))
+    const params = groups || {}
+    const matches = Object.values(matches)
+
+    matches.shift()
+
+    return { ...params, ...matches }
   }
 }
 
